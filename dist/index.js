ff75b6798f51d242f50cef49c5f5cef438f3b85c
Vishnu Ganesan <vishnukg@gmail.com>
2025-03-20 00:18:57 +1100

    more code

--- dist/index.js
+++ dist/index.js
@@ -4,3 +4,20 @@ const great = (who) => {
     console.log("Hello", who);
 };
 great("Vishnu");
+const states = [
+    {
+        name: "Alabama",
+        capital: "Montgomery",
+    },
+    {
+        name: "Alaska",
+        capital: "Juneau",
+    },
+    {
+        name: "Arizona",
+        capital: "Phoenix",
+    },
+];
+for (const state of states) {
+    console.log(state.capital);
+}
--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,22 @@ const great = (who: string) => {
 };
 
 great("Vishnu");
+
+const states = [
+    {
+        name: "Alabama",
+        capital: "Montgomery",
+    },
+    {
+        name: "Alaska",
+        capital: "Juneau",
+    },
+    {
+        name: "Arizona",
+        capital: "Phoenix",
+    },
+];
+
+for (const state of states) {
+    console.log(state.capital);
+}
